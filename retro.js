f503dc0a9909937475663337c1d6477a8deef7c9
linesh <linesh.simpcity@gmail.com>
2017-01-29 01:53:28 +0800

    Linesh: Update scripts

--- retro.js
+++ retro.js
@@ -1,1 +1,17 @@
 import TogglClient from 'toggl-api'
+import Settings from './settings'
+
+const apiToken = Settings.token
+let toggl = new TogglClient({ apiToken })
+
+toggl.getClients((error, clients) => {
+  console.log('----clients----')
+  console.log(clients)
+
+  clients.forEach(client => {
+    toggl.getClientProjects(client.id, true, (error, project) => {
+      console.log('----project----')
+      console.log(project)
+    })
+  })
+})
