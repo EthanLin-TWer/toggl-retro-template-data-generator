0b4c4253c0f598dca8b46e3a928f88a26150aec9
linesh <linesh.simpcity@gmail.com>
2017-01-31 13:54:21 +0800

    [#] Inline apiToken argv extraction and enable commented out tests

--- test/toggl.integration.spec.js
+++ test/toggl.integration.spec.js
@@ -1,16 +1,14 @@
 import { describe, before, it } from "mocha"
 import { expect } from 'chai'
-import minimist from 'minimist'
+import argv from 'minimist'
 import TogglClient from 'toggl-api'
 import Settings from '../settings'
 
 describe('Toggl API', () => {
    describe('toggl.summaryReport()', () => {
       let toggl, responseReport
-      let argv = minimist(process.argv.slice(2))
-      console.log(argv)
       before((done) => {
-         let apiToken = Settings.token || process.argv.apiToken
+         let apiToken = Settings.token || argv(process.argv.slice(2)).apiToken
          let options = {
             grouping: "clients", subgrouping: "projects",
             workspace_id: Settings.workspaceId,
@@ -20,33 +18,32 @@ describe('Toggl API', () => {
          toggl = new TogglClient({ apiToken })
          toggl.summaryReport(options, (error, reports) => {
             responseReport = reports
-            console.log(reports)
             done()
          })
       })
-      //
-      // it('response should have total_grand as millis and a data array', () => {
-      //    expect(responseReport.total_grand).to.be.a('number')
-      //    expect(responseReport.data).to.be.an('array')
-      // })
-      //
-      // it('for each client in response.data should have client id, client name, client total time grands and all project data as an \'items\' array', () => {
-      //    responseReport.data.forEach(client => {
-      //       expect(client.id).to.be.a('number')
-      //       expect(client.title.client).to.be.a('string')
-      //       expect(client.time).to.be.a('number')
-      //       expect(client.items).to.be.a('array')
-      //    })
-      // })
-      //
-      // it('for each project in response.data[x].items should have project name and project total time grands', () => {
-      //    let clientProjects = responseReport.data.map(client => client.items)
-      //    let projects = [].concat.apply([], clientProjects)
-      //   
-      //    projects.forEach(project => {
-      //       expect(project.title.project).to.be.a('string')
-      //       expect(project.time).to.be.a('number')
-      //    })
-      // })
+
+      it('response should have total_grand as millis and a data array', () => {
+         expect(responseReport.total_grand).to.be.a('number')
+         expect(responseReport.data).to.be.an('array')
+      })
+
+      it('for each client in response.data should have client id, client name, client total time grands and all project data as an \'items\' array', () => {
+         responseReport.data.forEach(client => {
+            expect(client.id).to.be.a('number')
+            expect(client.title.client).to.be.a('string')
+            expect(client.time).to.be.a('number')
+            expect(client.items).to.be.a('array')
+         })
+      })
+
+      it('for each project in response.data[x].items should have project name and project total time grands', () => {
+         let clientProjects = responseReport.data.map(client => client.items)
+         let projects = [].concat.apply([], clientProjects)
+
+         projects.forEach(project => {
+            expect(project.title.project).to.be.a('string')
+            expect(project.time).to.be.a('number')
+         })
+      })
    })
 })
