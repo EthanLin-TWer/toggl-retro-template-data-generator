0f159b2ef505df698a77e4754cabea69b2790b9e
linesh <linesh.simpcity@gmail.com>
2017-01-31 11:17:37 +0800

    [#4] Setup integration test suite

--- test/toggl.integration.spec.js
+++ test/toggl.integration.spec.js
@@ -1,8 +1,14 @@
 import { describe, beforeEach, it } from "mocha"
 import { expect } from 'chai'
+import TogglClient from 'toggl-api'
+import Settings from '../settings'
 
 describe('toggl api', () => {
    describe('toggl.summaryReport()', () => {
+      let toggl
+      beforeEach(() => {
+         toggl = new TogglClient({ apiToken: Settings.token })
+      })
       it('should return desired data structure', () => {
          
       })
