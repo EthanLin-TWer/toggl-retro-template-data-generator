3ab85eb6c933d5c4a75a1cd862f20877c27259c1
linesh <linesh.simpcity@gmail.com>
2017-01-31 11:13:19 +0800

    [#4] Add integration test for Toggl.summaryReport API

---
+++ test/toggl.integration.spec.js
@@ -0,0 +1,10 @@
+import { describe, beforeEach, it } from "mocha"
+import { expect } from 'chai'
+
+describe('toggl api', () => {
+   describe('toggl.summaryReport()', () => {
+      it('should return desired data structure', () => {
+         
+      })
+   })
+})
