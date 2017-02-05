24af5f443bf8c04022c797cda23b2b189e38b905
linesh <linesh.simpcity@gmail.com>
2017-02-05 22:32:41 +0800

    [#5] Refactor, extract total millis of one day and extract methods

--- src/time-service.js
+++ src/time-service.js
@@ -2,11 +2,12 @@ export class TimeService {
 
   constructor() {
     this.yyyymmddReg = /\d{4}-\d{2}-\d{2}/
+    this.MILLIS_OF_ONE_DAY = 24 * 60 * 60 * 1000
   }
 
   toMillis(days) {
     //           hours mins seconds millis
-    return Math.round(days * 24 * 60 * 60 * 1000)
+    return Math.round(days * this.MILLIS_OF_ONE_DAY)
   }
 
   millisToHoursAndMinsFormat(millis) {
@@ -21,18 +22,21 @@ export class TimeService {
     if (end.trim().length === 0) throw new Error('end date cannot be empty')
     if (!this.yyyymmddReg.test(start)) throw new Error('start date should be in yyyy-mm-dd format')
     if (!this.yyyymmddReg.test(end)) throw new Error('end date should be in yyyy-mm-dd format')
-    if (this.millisBetweenDays(start, end) < 0) throw new Error('start date can\'t be later than end date')
+    if (this.totalMillis(start, end) < this.millisOfOneDay()) throw new Error('start date can\'t be later than end date')
 
-    return this.millisToDay(this.millisBetweenDays(start, end)) + 1
+    return this.totalMillis(start, end) / this.millisOfOneDay()
   }
-
-  millisBetweenDays(start, end) {
-    return new Date(end) - new Date(start);
+  
+  totalMillis(start, end) {
+    return new Date(end) - new Date(start) + this.millisOfOneDay()
   }
-
+  
   millisToDay(millis) {
     //             seconds mins hours day
-    return millis / 1000 / 60 / 60 / 24
+    return millis / this.MILLIS_OF_ONE_DAY
   }
 
+  millisOfOneDay() {
+    return this.MILLIS_OF_ONE_DAY
+  }
 }
