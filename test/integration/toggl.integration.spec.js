8bf6c4c232e950367976f55f95d452591006d475
linesh <linesh.simpcity@gmail.com>
2017-02-12 07:56:44 +0800

    [#8] Validating data: should client time sum equals to returned total_grand

--- test/integration/toggl.integration.spec.js
+++ test/integration/toggl.integration.spec.js
@@ -4,7 +4,7 @@ import argv from 'minimist'
 import TogglClient from 'toggl-api'
 import Settings from '../settings.spec'
 
-describe('Toggl API: toggl.summaryReport', () => {
+describe('Toggl API: toggl.summaryReport(options, callback(err, reports))', () => {
   let toggl, responseReport
   before((done) => {
     let apiToken = Settings.token || argv(process.argv.slice(2)).apiToken
@@ -21,34 +21,44 @@ describe('Toggl API: toggl.summaryReport', () => {
     })
   })
 
-  it('response should have total_grand as millis and a data array', () => {
-    expect(responseReport.total_grand).to.be.a('number')
-    expect(responseReport.data).to.be.an('array')
-  })
-
-  it('for each client in response.data should have client id, client name, client total time grands and all project data as an \'items\' array', () => {
-    responseReport.data.forEach(client => {
-      expect(client.id).to.be.a('number')
-      expect(client.title.client).to.be.a('string')
-      expect(client.time).to.be.a('number')
-      expect(client.items).to.be.a('array')
+  describe('contract testing: data format should be the same as documented', () => {
+    it('response should have total_grand as millis and a data array', () => {
+      expect(responseReport.total_grand).to.be.a('number')
+      expect(responseReport.data).to.be.an('array')
     })
-  })
 
-  it('for each project in response.data[x].items should have project name and project total time grands', () => {
-    let clientProjects = responseReport.data.map(client => client.items)
-    let projects = [].concat.apply([], clientProjects) // black magic, flats a two dimension array
-
-    projects.forEach(project => {
-      expect(project.title.project).to.be.a('string')
-      expect(project.time).to.be.a('number')
+    it('for each client in response.data should have client id, client name, client total time grands and all project data as an \'items\' array', () => {
+      responseReport.data.forEach(client => {
+        expect(client.id).to.be.a('number')
+        expect(client.title.client).to.be.a('string')
+        expect(client.time).to.be.a('number')
+        expect(client.items).to.be.a('array')
+      })
     })
+
+    it('for each project in response.data[x].items should have project name and project total time grands', () => {
+      let clientProjects = responseReport.data.map(client => client.items)
+      let projects = [].concat.apply([], clientProjects) // black magic, flats a two dimension array
+
+      projects.forEach(project => {
+        expect(project.title.project).to.be.a('string')
+        expect(project.time).to.be.a('number')
+      })
+    })
+
   })
-
-  it('validate data logical correctness', () => {
-
+  
+  describe('logic testing: data should meet business logic for use', () => {
+    it('validate data logical correctness', () => {
+      let totalGrand = responseReport.total_grand
+      let totalClientTime = responseReport.data.map(client => client.time)
+        .reduce((sum, time) => sum + time, 0)
+      
+      expect(totalClientTime).to.equal(totalGrand)
+    })
+    
   })
-
+  
   after('', () => {
     console.log(`Response: \n${JSON.stringify(responseReport, null, '  ')}`)
   })
